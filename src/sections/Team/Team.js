8e10210de55e6ff9120d86332e39bf59fc2f2100
Aline Souza <alinesouzadev@gmail.com>
2021-10-18 13:57:26 +0100

    Add Team changes

--- src/sections/Team/Team.js
+++ src/sections/Team/Team.js
@@ -1,11 +1,58 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 
 const Team = () => {
   return (
-    <Box>
-      Team
-    </Box>
+    <Grid container>
+      {/* <Grid item>
+        <Typography>
+          Our Team
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Grid container>
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Card img={img1} title={title} text={text} />
+              </Grid>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+              <Grid item>
+                <Card img={img1} title={ } text={text} />
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid> */}
+    </Grid>
   );
 }
 export default Team
