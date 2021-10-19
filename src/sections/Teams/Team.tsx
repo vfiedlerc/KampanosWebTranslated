5ebaf1c8981a8b6d642c57b126b985323c2e8cd4
Aline Souza <alinesouzadev@gmail.com>
2021-10-19 15:20:28 +0100

    Update Team section MediaCard component

--- src/sections/Teams/Team.tsx
+++ src/sections/Teams/Team.tsx
@@ -11,13 +11,26 @@ import joao from '../../assets/Joao.jpg';
 import eva from '../../assets/Eva.jpg';
 import aline from '../../assets/Aline.jpg';
 import bruno from '../../assets/Bruno.jpg';
+import Button from '@mui/material/Button';
+import OurTeamEffect from '../../assets/OurTeamEffect.png';
+import OurTeamEffects from '../../assets/OurTeamEffect.svg';
+
+
+
 
 export const Team: FC<TeamProps> = ({
   props
 }: TeamProps) => {
   return (
+    // <Box sx={{backgroundImage: `${OurTeamEffects}`, backgroudSize: 'cover', backgroundRepeat: 'no-repeat'}}> 
+
     <Grid container spacing={2} >
-      <Grid item md={3}>
+
+      
+      <Grid item xs={12} md={4}>
+
+     
+
         <Typography
         sx={{
           transform: 'rotate(-90deg)',
@@ -27,53 +40,62 @@ export const Team: FC<TeamProps> = ({
         >
           Our Team
         </Typography> 
+
+
       </Grid>
-      <Grid item md={9}>
+      <Grid item xs={6} md={8}>
         <Grid container spacing={2} columns={3} rowSpacing={6}>
           <Grid item >
             <Grid container spacing={2} >
               <Grid item>
+
                 <MediaCard image={andreas} title='Andreas Thomae' text='CEO & Chief of Development' />
+                
+                
               </Grid>
               <Grid item>
-                <MediaCard image={rafael} title='Rafael Facadas' text='UX/UI & Visual Designer' />
+
+                <MediaCard image={andreas} title='Rafael Facadas' text='UX/UI & Visual Designer' />
+
               </Grid>
               <Grid item>
-                <MediaCard image={michael} title='Michael Wiederkehr' text='Key-Account Manager' />
+
+                <MediaCard image={andreas} title='Michael Wiederkehr' text='Key-Account Manager' />
               </Grid>
             </Grid>
           </Grid>
           <Grid item>
             <Grid container spacing={2} >
               <Grid item>
-                <MediaCard image={vinicius} title='Vinícius Fiedler' text=' Full-Stack Developer' />
+                <MediaCard image={andreas} title='Vinícius Fiedler' text='  Full-Stack Developer' />
               </Grid>
               <Grid item>
-                <MediaCard image={guilherme} title='Guilherme Lemos' text='UX/UI & Visual Designer' />
+                <MediaCard image={andreas} title='Guilherme Lemos' text='UX/UI & Visual Designer' />
               </Grid>
               <Grid item>
-                <MediaCard image={joao} title='João Bugarin' text='Software Developer' />
+                <MediaCard image={andreas} title='João Bugarin' text='Software Developer' />
               </Grid>
             </Grid>
           </Grid>
           <Grid item>
             <Grid container spacing={2}>
               <Grid item>
-                <MediaCard image={eva} title='Eva Dias' text='UX/UI & Visual Designer' />
+                <MediaCard image={andreas} title='Eva Dias' text='UX/UI & Visual Designer' />
               </Grid>
               <Grid item>
-                <MediaCard image={aline} title='Aline Souza' text='Software Developer' />
+                <MediaCard image={andreas} title='Aline Souza' text='Software Developer' />
               </Grid>
               <Grid item>
-                <MediaCard image={bruno} title='Bruno Buss' text='Web & Front-end Developer' />
+                <MediaCard image={andreas} title='Bruno Buss' text='Web & Front-end Developer' />
               </Grid>
             </Grid>
           </Grid>
         </Grid>
       </Grid>
     </Grid>
+    // </Box>
   );
 }
 export type TeamProps = {
   props: any
-};
+}
