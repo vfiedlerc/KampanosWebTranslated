c8fa80990893b1f3fa933cbf3c1909d800789025
Aline Souza <alinesouzadev@gmail.com>
2021-10-19 15:35:29 +0100

    Changed Title component

--- src/components/Titles/Title.tsx
+++ src/components/Titles/Title.tsx
@@ -1,19 +1,58 @@
 import * as React from 'react';
 import { FC } from 'react'
-import { Box, Typography } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
+
+const TitleBox = styled('div')(({ theme }) => ({
+  margin: 0,
+  padding: 0,
+  [theme.breakpoints.down('sm')]: {
+    h1: {
+      transform: 'none',
+      fontSize: '2.35rem',
+      textAlign: 'left',
+      textAlignLast: 'left',
+      marginLeft: '1rem',
+      marginTop: '2rem',
+      marginBottom: '2rem'
+    }
+  },
+  [theme.breakpoints.between('sm', 'md')]: {
+    h1: {
+      transform: 'none',
+      fontSize: '3rem',
+      textAlign: 'left',
+      textAlignLast: 'left',
+      marginLeft: '1rem',
+      marginTop: '2rem',
+      marginBottom: '2rem'
+    }
+  },
+  [theme.breakpoints.between('md', 'lg')]: {
+    h1: {
+      transform: 'rotate(-90deg)',
+      fontSize: '4rem'
+    }
+  },
+  [theme.breakpoints.up('lg')]: {
+    h1: {
+      transform: 'rotate(-90deg)',
+      fontSize: '5rem'
+    }
+  }
+}));
 
 export const Title: FC<TitleProps> = ({
   label,
   color
 }: TitleProps) => {
   return (
-    <Box>
+    <TitleBox>
       <Typography
         component='h1'
         variant='h2'
         sx={{
           color: `${color}`,
-          WebkitTextFillColor: 'transparent', 
+          WebkitTextFillColor: 'transparent',
           WebkitTextStrokeWidth: '0.5px',
           WebkitTextStrokeColor: `${color}`,
           fontWeight: 'bold'
@@ -21,7 +60,7 @@ export const Title: FC<TitleProps> = ({
       >
         {label}
       </Typography>
-    </Box>
+    </TitleBox>
   )
 }
 export type TitleProps = {
