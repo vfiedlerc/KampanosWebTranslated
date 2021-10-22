ca3580d428ebc9d038340242838f33acafdaac1f
Aline Souza <alinesouzadev@gmail.com>
2021-10-22 13:46:13 +0100

    Changes Drawer

--- src/components/Drawer/DrawerMenu.tsx
+++ src/components/Drawer/DrawerMenu.tsx
@@ -63,212 +63,214 @@ export const DrawerMenu: FC<DrawerMenuProps> = ({
             </Toolbar>
           </AppBar>
 
-
-
-
-          <Grid container spacing={2} direction="column"
+          <Grid container
+            spacing={2}
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+            direction="row"
             justifyContent="center"
             alignItems="flex-start"
-
-
+            sx={{height: '100%'}}
           >
-
-            <Grid item direction="column">
-
-
-              <Grid container>
+            {/* Redes */}
+            <Grid item
+              xs={2}
+              sm={2}
+              md={2}
+              lg={2}>
+              <Grid container
+                spacing={2}
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+                direction="row"
+                justifyContent="center"
+                alignItems="flex-end"
+              >
                 <Grid item>
-
-
-
-                  <Grid container direction="column">
-
-                    <Grid item xs={12}>  
-
-                    <Typography sx={{ transform: 'rotate(-90deg)' }} color=" #FAFAFA">
-                      
-                       @kampanos 2021
-
-                     </Typography></Grid>
-
-
-
-                    <Grid item xs={12}>
-                      <img src={FaceIcon} alt="Facebook" title="Facebook" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                      <img src={LinkIcon} alt="Facebook" title="Facebook" />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                      <img src={InstaIcon} alt="Facebook" title="Facebook" />
+                  <Grid container>
+                    <Grid item direction="column">
+                      <Grid container>
+                        <Grid item>
+                          <Grid container direction="column">
+                            <Grid item xs={12}>
+                              <Typography sx={{ transform: 'rotate(-90deg)' }} color=" #FAFAFA">
+                                @kampanos 2021
+                              </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                              <img src={FaceIcon} alt="Facebook" title="Facebook" />
+                            </Grid>
+                            <Grid item xs={12}>
+                              <img src={LinkIcon} alt="Facebook" title="Facebook" />
+                            </Grid>
+                            <Grid item xs={12}>
+                              <img src={InstaIcon} alt="Facebook" title="Facebook" />
+                            </Grid>
+                          </Grid>
+                        </Grid>
+                      </Grid>
                     </Grid>
                   </Grid>
-
                 </Grid>
+              </Grid>
+            </Grid>
 
-                <Grid item direction="column">
-
-
-
+            <Grid item
+              xs={10}
+              sm={10}
+              md={10}
+              lg={10}>
+              {/* Menu */}
+              <Grid container
+                spacing={2}
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+                direction="row"
+                justifyContent="left"
+                alignItems="flex-start"
+               
+              >
+                <Grid item>
+                  <Grid container direction='column'>
+                    <Grid item>
+                      <Box>
+                        <Typography
+                          variant="h2"
+                          sx={{
+                            marginRight: 0
+                          }}
+                        >
+                          <MenuItem
+                            label='Home'
+                            href='#'
+                            title='Home'
+                            target='_self'
+                            color="#878787"
+                          />
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item>
+                      <Box
+                        sx={{
+                          justifyContent: "center",
+                        }}
+                      >
+                      </Box>
+                      <Box>
+                        <Typography
+                          variant="h2"
+                          sx={{
+                            marginRight: 0
+                          }}
+                        >
+                          <MenuItem
+                            label='About us'
+                            href='#'
+                            title='About us'
+                            target='_self'
+                            color="#878787"
+                          />
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item>
+                      <Box>
+                        <Typography
+                          variant="h2"
+                          sx={{
+                            marginRight: 0
+                          }}
+                        >
+                          <MenuItem
+                            label='Our services'
+                            href='#'
+                            title='Our services'
+                            target='_self'
+                            color="#878787"
+                          />
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item>
+                      <Box>
+                        <Typography
+                          variant="h2"
+                          sx={{
+                            marginRight: 0
+                          }}
+                        >
+                          <MenuItem
+                            label='Our story'
+                            href='#'
+                            title='Our story'
+                            target='_self'
+                            color="#878787"
+                          />
+                        </Typography>
+                      </Box>
+                    </Grid>
+                    <Grid item>
+                      <Box>
+                        <Typography
+                          variant="h2"
+                          sx={{
+                            marginRight: 0
+                          }}
+                        >
+                          <MenuItem
+                            label='Our team'
+                            href='#'
+                            title='Our team'
+                            target='_self'
+                            color="#CEE8F9"
+                          />
+                        </Typography>
+                      </Box>
+                    </Grid>
+                  </Grid>
                 </Grid>
-
               </Grid>
 
-
-
-            </Grid>
-            <Grid item>
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    marginRight: 0,
-
-                  }}
-                >
-                  <MenuItem
-                    label='Home'
-                    href='#'
-                    title='Home'
-                    target='_self'
-                    color="#878787"
-                  />
-                </Typography>
-              </Box>
-            </Grid>
-
-
-
-
-            <Grid item>
-
-              <Box
+              {/* Let's work together */}
+              <Grid container
+                xs={12}
+                sm={12}
+                md={12}
+                lg={12}
+                direction="row"
                 sx={{
-                  justifyContent: "center",
+                  height: '250px',
+                  alignContent: 'end',
+                  justifyContent: "right",
+                  alignItems: "bottom"
                 }}
               >
-              </Box>
-
-
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    marginRight: 0,
-
-
-                  }}
-                >
-                  <MenuItem
-                    label='About us'
-                    href='#'
-                    title='About us'
-                    target='_self'
-                    color="#878787"
-                  />
-                </Typography>
-              </Box>
-            </Grid>
-
-
-
-
-
-
-
-
-            <Grid item>
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    marginRight: 0,
-
-                  }}
-                >
-                  <MenuItem
-                    label='Our services'
-                    href='#'
-                    title='Our services'
-                    target='_self'
-                    color="#878787"
-                  />
-                </Typography>
-              </Box>
-
-            </Grid>
-
-
-
-
-
-            <Grid item>
-
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    marginRight: 0,
-
-                  }}
-                >
-                  <MenuItem
-                    label='Our story'
-                    href='#'
-                    title='Our story'
-                    target='_self'
-                    color="#878787"
-                  />
-                </Typography>
-              </Box>
-            </Grid>
-
-
-
-
-            <Grid item>
-              <Box>
-                <Typography
-                  variant="h2"
-                  sx={{
-                    marginRight: 0,
-
-                  }}
-                >
-                  <MenuItem
-                    label='Our team'
-                    href='#'
-                    title='Our team'
-                    target='_self'
-                    color="#CEE8F9"
-                  />
-                </Typography>
-              </Box>
-            </Grid>
-
-            <Grid container direction="row"
-              justifyContent="flex-end"
-              alignItems="flex-end" spacing={3}>
-
-
-              <Grid item>
-                <Typography variant="h3" color="#FAFAFA">Let’s work together!</Typography>
+                <Grid item>
+                  <Grid container>
+                    <Grid container direction="row"
+                      justifyContent="flex-end"
+                      alignItems="flex-end" spacing={3}>
+                      <Grid item>
+                        <Typography variant="h3" color="#FAFAFA">Let’s work together!</Typography>
+                      </Grid>
+                      <Grid item>
+                        <img src={rectangleMenu} alt="Facebook" title="Facebook" />
+                      </Grid>
+                      <Grid item>
+                        <img src={workMenuButton} alt="Facebook" title="Facebook" />
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
               </Grid>
-
-
-
-              <Grid item>
-                <img src={rectangleMenu} alt="Facebook" title="Facebook" />
-              </Grid>
-
-              <Grid item>
-                <img src={workMenuButton} alt="Facebook" title="Facebook" />
-              </Grid>
-
             </Grid>
-
 
           </Grid>
         </Box>
