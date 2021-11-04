a44ce9275b1e2aaee8289ba0fa404ddb84c5f8e3
Aline Souza <alinesouzadev@gmail.com>
2021-11-04 15:04:05 +0000

    FIX: use emailJS to send data

--- src/Contexts/formContext.tsx
+++ src/Contexts/formContext.tsx
@@ -2,11 +2,18 @@ import { AxiosResponse } from "axios";
 import { useState } from "react";
 import { createContext, ReactNode } from "react";
 import { api } from "../service/index";
+import { send } from 'emailjs-com';
+import { init } from 'emailjs-com';
+
+// 
+//Add the API USER value (check emailJS account config)
+//
+//init("USER_API");
 
 interface FormContextData {
-  sendFormData:  () => Promise<Response>
+  sendFormData: () => Promise<Response>
   setFirstName: React.Dispatch<React.SetStateAction<string>>
-  setLastName: React.Dispatch<React.SetStateAction<string>>  
+  setLastName: React.Dispatch<React.SetStateAction<string>>
   setCompany: React.Dispatch<React.SetStateAction<string>>
   setProject: React.Dispatch<React.SetStateAction<string>>
   setBudget: React.Dispatch<React.SetStateAction<string>>
@@ -46,32 +53,84 @@ export function FormProvider({ children }: FormProviderProps) {
 
   const [pageNumber, setPageNumber] = useState<number>()
 
+  const [toSend, setToSend] = useState({
+    from_name: email,
+    to_name: '',
+    message: {},
+    reply_to: ''
+  });
+
   // send data to api
-  const sendFormData = async () => {
+  async function sendFormData() {
     const formData = [
       { name: "first_name", value: firstName },
-      { name: "last_name", value: lastName },     
+      { name: "last_name", value: lastName },
       { name: "company", value: company },
       { name: "project", value: project },
       { name: "budget", value: budget },
       { name: "email", value: email },
       { name: "phone", value: phone }
-    ]
+    ];
 
-    return new Promise<Response>(async(resolve, reject) => {
-      try {      
-        const response = await api.post('',formData)
-        resolve({response,formData})
-      } catch (error) {
-        reject(error)
+    //
+    // Set the email where the data will be sent (to_name:'sales@kampanos.pt')
+    //
+    setToSend(
+      {
+        ...toSend,
+        from_name: email,
+        //to_name: 'EMAIL_ENVIO_TESTE',
+        message: [
+          {'Name:': firstName},
+          {'Email:': email},
+          {'Phone:': phone},
+          {'Company:': company},
+          {'Budget:': budget},
+          {'Project:': project}
+        ],
+        reply_to: ''
       }
-    })    
+    )
+
+    return new Promise<any>(async (resolve, reject) => {
+      try {
+        console.log(formData);
+        console.log(toSend);
+
+        const response = formData
+        resolve({response,formData})
+
+        //
+        // Send data to email throught emailJS
+        //
+        // send(
+        //   'SERVICE ID',
+        //   'TEMPLATE ID',
+        //   toSend,
+        //   'User ID'
+        // )
+        //   .then((response) => {
+        //     console.log('SUCCESS!', response.status, response.text);
+        //   })
+        //   .catch((err) => {
+        //     console.log('FAILED...', err);
+        //   });
+
+
+        // //To use after backend is configured
+        // const response = await api.post('',formData)
+        // resolve({response,formData})
+
+      } catch (error) {
+        reject(error);
+      }
+    });
   }
 
   return (
     <formContext.Provider
       value={{
-        sendFormData,        
+        sendFormData,
         setFirstName,
         setLastName,
         setCompany,
@@ -79,18 +138,17 @@ export function FormProvider({ children }: FormProviderProps) {
         setBudget,
         setEmail,
         setPhone,
-        setPageNumber,       
+        setPageNumber,
         pageNumber,
         firstName,
         lastName,
         company,
         project,
         budget,
-        email,       
+        email,
         phone
       }}>
       {children}
     </formContext.Provider>
   )
-
 }
--- src/components/Form/Details/index.tsx
+++ src/components/Form/Details/index.tsx
@@ -1,36 +1,53 @@
-import React, { FC, useContext, useEffect, useState } from 'react'
+import React, { FormEvent, useContext, useEffect, useRef, useState } from 'react'
 import { formContext } from "../../../Contexts/formContext"
 import { Box, Button, TextField, Typography } from '@mui/material'
 import { useHistory } from 'react-router'
 import NextButton from '../../Button/NextButton';
 import validator from 'validator'
+import Project from '../Project';
 
 function Details() {
     const [email, setEmailInput] = useState("")
     const [phone, setPhoneInput] = useState("")
-    const [isEmailValid, setIsEmailValid] = useState(true);
-    const { setEmail, setPhone } = useContext(formContext);
-
-    const emailRegex = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i;
-
-    const testEmail = emailRegex.test(email)
-    const validateEmail = validator.isEmail(email)    
-   
+    const [errorType, setErrorType] = useState('')
+    const [isDataSent, setIsDataSent] = useState(false)
+    const { setEmail, setPhone, sendFormData } = useContext(formContext);
 
     const history = useHistory();
 
-    const handleClickNext = () => {
+    const handleSubmit = async (event: FormEvent) => {
+        console.log(errorType)
+        event.preventDefault()
+
+        const emailRegex = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i;
+
+        const testEmail = emailRegex.test(email)
+        const validateEmail = validator.isEmail(email)
         if (testEmail === true && validateEmail === true) {
-            setIsEmailValid(true)  
             setEmail(email)
             setPhone(phone)
+            setErrorType('')
+            setIsDataSent(true)
             console.log(email)
             console.log(phone)
-            history.push("/contact/name");    
         } else {
-            setIsEmailValid(false)
+            setErrorType('Invalid email')
         }
     };
+
+    useEffect(() => {
+        if (isDataSent) {
+            (async () => {
+                try {
+                    await sendFormData()                    
+                    //history.push("/contact/thankyou")
+                } catch (error) {
+                    console.error(error)
+                }
+            })()
+            setIsDataSent(() => false)
+        }
+    }, [isDataSent, sendFormData, setIsDataSent])
 
     useEffect(() => {
         window.scrollTo({
@@ -39,8 +56,13 @@ function Details() {
         });
     });
 
+
     return (
         <Box
+            component='form'
+            onSubmit={handleSubmit}
+            // onSubmit={(e: any) => handleSubmit(e)}
+
             sx={{
                 justifyContent: 'center',
                 alignSelf: 'center',
@@ -49,7 +71,8 @@ function Details() {
                 '& > :not(style)': { m: 1.5, width: '30ch' },
                 '& .MuiInputBase-root:before': { color: '#ED6C02', borderBottom: '1px solid #ED6C02' },
                 '& .MuiInputBase-root:hover:not(disabled):before': { color: '#ED6C02', borderBottom: '1px solid #ED6C02' }
-            }}>
+            }}
+        >
             <Typography
                 variant='body2'
                 sx={{
@@ -74,6 +97,7 @@ function Details() {
                 color="warning"
                 value={email}
                 onChange={(e) => setEmailInput(e.target.value)}
+                type='text'
             />
             <TextField name="phone"
                 label="phone"
@@ -81,9 +105,12 @@ function Details() {
                 color="warning"
                 value={phone}
                 onChange={(e) => setPhoneInput(e.target.value)}
+                type='text'
             />
 
-            <NextButton onClick={handleClickNext} />
+            {/* <NextButton type='submit' onSubmit={(event: any) => handleSubmit(event)}  /> */}
+            {/* <NextButton onClick={handleSubmit}/> */}
+            <NextButton type='submit' />
         </Box>
     );
 };
